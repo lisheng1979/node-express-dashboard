249ce9c41fd614b7583b364426f72cd1fea34440
lisheng1979 <skyeyes@263.net>
2020-06-05 21:52:04 -0700

    Completed module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const fileService = require("../services/select-file-service")
-const { getSettings, writeSettings, isValidDir } = require("../services/settings-service.js");
+const fileService = require("../services/select-file-service");
+const {
+  getSettings,
+  writeSettings,
+  isValidDir,
+} = require("../services/settings-service.js");
 const { validationResult } = require("express-validator");
 const { body } = require("express-validator");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Log Dashboard" });
+  res.render("index", { title: "Log Dashboard", logFile: req.query.logFile });
 });
 
 /* GET select file. */
@@ -20,33 +24,37 @@ router.get("/settings", (req, res, next) => {
   res.render("settings", { title: "Settings", settings: getSettings() });
 });
 
-router.post("/settings", [
-  body("defaultDir").custom(dirPath => {
-    if (dirPath && !isValidDir(dirPath)) {
-      throw new Error("Default directory is not valid")
+router.post(
+  "/settings",
+  [
+    body("defaultDir").custom((dirPath) => {
+      if (dirPath && !isValidDir(dirPath)) {
+        throw new Error("Default directory is not valid");
+      }
+      return true;
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("settings", {
+        title: "Settings",
+        errors: errors.array()[0].msg,
+        settings: getSettings(),
+      });
     }
-    return true
-  })
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.render("settings", { 
-      title: "Settings", 
-      errors: errors.array()[0].msg, 
-      settings: getSettings() 
+
+    const saved = writeSettings(req.body);
+    res.render("settings", {
+      message: saved ? "Settings Saved" : "",
+      errors: saved ? "" : "Settings not saved",
+      title: "Settings",
+      settings: getSettings(),
     });
   }
-
-  const saved = writeSettings(req.body)
-  res.render("settings", { 
-    message: saved ? "Settings Saved" : "", 
-    errors: saved ? "" : "Settings not saved",
-    title: "Settings", 
-    settings: getSettings() 
-  });
-})
+);
 
 /* GET files. */
-router.get("/files", (req, res) => fileService.get(req, res))
+router.get("/files", (req, res) => fileService.get(req, res));
 
 module.exports = router;
