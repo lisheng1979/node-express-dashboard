aedbad39d8bb6864fa58b5683152266896430e46
lisheng1979 <skyeyes@263.net>
2020-06-05 22:14:06 -0700

    Completed module

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,10 +1,11 @@
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
-const filePath = document.getElementById("logFilePath");
+const filePath = document.getElementById("logFilePath").value;
 connection.onopen = () => {
   if (filePath) connection.send(filePath);
 };
 
 connection.onmessage = (event) => {
-  logWindow.innerHTML = event.data;
+  const logs = event.data.split("\n").join("<hr>");
+  logWindow.innerHTML = logs;
 };
