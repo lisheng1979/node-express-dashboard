75a2365d738910ed8bb9d86c212dc49abc87fc66
lisheng1979 <skyeyes@263.net>
2020-06-05 23:47:57 -0700

    Completed module

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,19 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const settingsFilePath = path.join(__dirname, "../json/settings.json");
 function getSettings() {
-  return {}
+  const settingsDate = fs.readFileSync(settingsFilePath);
+  return JSON.parse(settingsDate);
 }
 
-function writeSettings(newSettings) {
-}
+function writeSettings(newSettings) {}
 
-function getDefaultDir() {
-}
+function getDefaultDir() {}
 
-function isValidDir(dirPath) {
-}
+function isValidDir(dirPath) {}
 
 module.exports = {
   getSettings,
   writeSettings,
   getDefaultDir,
-  isValidDir
+  isValidDir,
 };
